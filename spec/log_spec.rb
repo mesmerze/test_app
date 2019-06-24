cacec5b14d60a14316560474efc43070905d90b1
Alexei Charnou <Charnou@epam.com>
2019-06-24 17:46:41 +0300

    Fixed specs for log printer

--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -4,11 +4,11 @@ require 'log'
 require 'spec_helper'
 
 RSpec.describe Log do
-  subject { described_class.new(logfile).webpages }
+  subject { described_class.new(logfile) }
 
   let(:logfile) { 'spec/fixtures/sample.log' }
 
   it 'receives logfile as input and process it' do
-    expect( subject).to eq( {"/help_page/1"  => ['126.318.035.038'], "/contact" => ['184.123.665.067'], "/home" => ['184.123.665.067']})
+    expect(subject.webpages).to eq ( { '/help_page/1' => ['126.318.035.038'], '/contact' => ['184.123.665.067'], '/home' => ['184.123.665.067'] })
   end
 end
